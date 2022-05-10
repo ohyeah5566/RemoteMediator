3e17e0da2678e0e07dd4b181fb32d4cf1699c821
ohyeah5566 <ohyeah5566@gmail.com>
2022-05-10 16:49:01 +0800

    init

---
+++ app/src/main/java/com/ohyeah5566/remotemediator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ohyeah5566.remotemediator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
